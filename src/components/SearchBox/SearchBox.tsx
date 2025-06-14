7a77a7b0c32a9fe99a8a05488c1a2e2fae56e9a7
irinasvetlichnaya <irinasvetlichnaya16@gmail.com>
2025-06-15 00:48:50 +0300

    added structure

---
+++ src/components/SearchBox/SearchBox.tsx
@@ -0,0 +1,14 @@
+import css from './SearchBox.module.css';
+
+export default function name() {
+
+    return (
+        <input
+	className={css.input}
+  type="text"
+  placeholder="Search notes"
+ />
+
+    )
+   
+}
